6db6eb3deedf827c8a4f2a1bdc25b29081e52efb
eli <elipoultorak@gmail.com>
2016-08-29 20:36:44 +0300

    Initial test suite

---
+++ tests/GetOrdersTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(__DIR__ . '/setupTests.php');
+
+class GetOrdersTest extends PHPUnit_Framework_TestCase {
+	public function test(){
+		//global $testApiKey;
+		$this->assertTrue(true);
+		/*$smartrade = new Smartrade\Smartrade($testApiKey);
+		$smartrade->getOrders();*/
+	}
+}
